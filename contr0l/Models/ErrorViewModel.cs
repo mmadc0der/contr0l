68dc04a78e5e9d56ab9cde5395a16ac980aeca82
mmadc0der <tim110702@mail.ru>
2024-03-24 16:30:47 +0300

    Add project files.

---
+++ contr0l/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace contr0l.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
